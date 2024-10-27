12eb76a169c59aebb267aef454e248ebd9ca712e
Rehan George <rehan.george@gmail.com>
2024-10-27 18:08:21 +0530

    Add structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,34 @@
+struct User {
+    active: bool,
+    username: String,
+    sign_in_count: u32,
+}
+
+// Tuple struct
+struct Coordinates(i32, i32, i32);
+
+// Unit struct
+struct UnitStruct;
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User{
+        active: true,
+        username: String::from("Rehan"),
+        sign_in_count: 0,
+    };
+
+    println!("{}", user1.username);
+
+    let user2 = build_user(String::from("George"));
+    println!("{}", user2.username);
+
+    let coords = Coordinates(1, 2, 3);
+}
+
+fn build_user(username: String) -> User {
+    User {
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }

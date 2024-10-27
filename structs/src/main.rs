63907887b43affa0742ed790047dc68ba5dcb100
Rehan George <rehan.george@gmail.com>
2024-10-27 18:13:41 +0530

    Add Square

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -10,6 +10,25 @@ struct Coordinates(i32, i32, i32);
 // Unit struct
 struct UnitStruct;
 
+struct Square {
+    width: u32,
+    height: u32,
+}
+
+impl Square {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn whats_my_area(&self) {
+        println!("The area of the square is: {}", self.area());
+    }
+
+    fn change_width(&mut self, new_width: u32) {
+        self.width = new_width;
+    }
+}
+
 fn main() {
     let user1 = User{
         active: true,
@@ -23,6 +42,12 @@ fn main() {
     println!("{}", user2.username);
 
     let coords = Coordinates(1, 2, 3);
+
+    let mut sq = Square { width: 10, height: 10 };
+    sq.whats_my_area();
+
+    sq.change_width(20);
+    sq.whats_my_area();
 }
 
 fn build_user(username: String) -> User {
